50d7d03ed620d0a552395cc8510cbdb311340c77
impudding <janehsu87211@gmail.com>
2019-03-14 23:29:49 +0800

    add modified all-data of max

--- eyeAndAd.py
+++ eyeAndAd.py
@@ -11,5 +11,5 @@ import seaborn as sns
 from statsmodels.stats.anova import AnovaRM
 
 #https://github.com/impudding/Maximizer-Saticeficer/blob/master/data/eye_and_adQuestionaire_maximizer.csv
-df_m = pandas.read_csv("https://github.com/impudding/Maximizer-Saticeficer/blob/master/data/eye_and_adQuestionaire_maximizer.csv")
+df_m = pd.read_csv("https://github.com/impudding/Maximizer-Saticeficer/blob/master/data/eye_and_adQuestionaire_maximizer.csv",error_bad_lines=False)
 sns.relplot(x='ad_recog',y='total_fix_dur_avr',data=df_m)
