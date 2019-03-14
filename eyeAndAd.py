837ef0a421d43e3013cdcef5f9bd42799da34a4d
impudding <janehsu87211@gmail.com>
2019-03-14 23:23:33 +0800

    add modified eye_and_ad_max data

---
+++ eyeAndAd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 12:22:44 2019
+
+@author: yun-chen
+"""
+
+import pandas as pd
+import seaborn as sns
+from statsmodels.stats.anova import AnovaRM
+
+#https://github.com/impudding/Maximizer-Saticeficer/blob/master/data/eye_and_adQuestionaire_maximizer.csv
+df_m = pandas.read_csv("https://github.com/impudding/Maximizer-Saticeficer/blob/master/data/eye_and_adQuestionaire_maximizer.csv")
+sns.relplot(x='ad_recog',y='total_fix_dur_avr',data=df_m)
